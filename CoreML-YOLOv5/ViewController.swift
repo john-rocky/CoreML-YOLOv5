ed67e35a17689697fc6192331dc708e65efda371
john-rocky <john-rocky@hubot.com>
2021-12-28 04:13:31 +0900

    Initial Commit

---
+++ CoreML-YOLOv5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreML-YOLOv5
+//
+//  Created by 間嶋大輔 on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
